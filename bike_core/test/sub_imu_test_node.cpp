e8cae6f0d2464835d7d8b982adc98f2cdd51cb4d
weihao <weihao_ysgs@163.com>
2022-09-19 20:03:01 +0800

    test node success

---
+++ bike_core/test/sub_imu_test_node.cpp
@@ -0,0 +1,19 @@
+#include <glog/logging.h>
+#include <ros/ros.h>
+
+void InitGlog() {
+  google::InitGoogleLogging("Remote Control Node");
+  google::SetLogFilenameExtension("log_");
+  google::SetLogDestination(google::INFO, "log/");
+
+  FLAGS_stderrthreshold = google::INFO;
+  FLAGS_colorlogtostderr = true;
+  FLAGS_max_log_size = 1024;
+  FLAGS_stop_logging_if_full_disk = true;
+}
+
+int main(int argc, char** argv) {
+  InitGlog();
+  ros::init(argc, argv, "sub_imu_test_node");
+  LOG(INFO) << "Hello Node";
+}
