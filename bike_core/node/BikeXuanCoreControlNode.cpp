6e055311a0165253c65a196ad1b27bd940d09f24
weihao <weihao_ysgs@163.com>
2022-10-02 14:06:26 +0800

    add glog init fun

--- bike_core/node/BikeXuanCoreControlNode.cpp
+++ bike_core/node/BikeXuanCoreControlNode.cpp
@@ -1,6 +1,15 @@
 #include "bike_core/BikeXuanControl.hpp"
-
+void InitGlog() {
+  google::InitGoogleLogging("bike_core_control_node");
+  google::SetLogFilenameExtension("log_");
+  google::SetLogDestination(google::INFO, "log/");
+  FLAGS_stderrthreshold = google::INFO;
+  FLAGS_colorlogtostderr = true;
+  FLAGS_max_log_size = 1024;
+  FLAGS_stop_logging_if_full_disk = true;
+}
 int main(int argc, char** argv) {
+  InitGlog();
   ros::init(argc, argv, "bike_core_control_node");
   BikeXuanControl bike_xuan_core_control;
   ros::spin();
--- bike_core/node/CanSendReceiveNode.cpp
+++ bike_core/node/CanSendReceiveNode.cpp
@@ -1,6 +1,15 @@
 #include "bike_core/CanReceiveSend.hpp"
-
+void InitGlog() {
+  google::InitGoogleLogging("can_send_receive_node");
+  google::SetLogFilenameExtension("log_");
+  google::SetLogDestination(google::INFO, "log/");
+  FLAGS_stderrthreshold = google::INFO;
+  FLAGS_colorlogtostderr = true;
+  FLAGS_max_log_size = 1024;
+  FLAGS_stop_logging_if_full_disk = true;
+}
 int main(int argc, char** argv) {
+  InitGlog();
   ros::init(argc, argv, "can_send_receive_node");
   CanSendReceive can;
   ros::spin();
