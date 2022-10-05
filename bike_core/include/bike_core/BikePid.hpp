04ab5acd89459292be67148355b0cc26e26b1384
weihao <weihao_ysgs@163.com>
2022-10-05 14:37:47 +0800

    success read pid params from yaml file

--- bike_core/include/bike_core/BikePid.hpp
+++ bike_core/include/bike_core/BikePid.hpp
@@ -4,11 +4,30 @@
 #include <glog/logging.h>
 
 #include <memory>
+#include <opencv2/opencv.hpp>
 
 struct PidParams {
-  PidParams(float kp, float ki, float kd) : kp_(kp), ki_(ki), kd_(kd){};
-  float kp_{0.0}, ki_{0.0}, kd_{0.0};
-  float last_error_{0.0};
+  PidParams(float kp, float ki, float kd)
+      : kp_(kp), ki_(ki), kd_(kd), pid_name_(""){};
+  PidParams(const std::string& pid_parmas_file) {
+    cv::FileStorage file(pid_parmas_file, cv::FileStorage::READ);
+    LOG_IF(FATAL, !file.isOpened())
+        << "File: " << pid_parmas_file << "Is Not Found";
+    file["Kp"] >> kp_;
+    file["Ki"] >> ki_;
+    file["Kd"] >> kd_;
+    file["Pid.Name"] >> pid_name_;
+    file["Output.Limit"] >> output_limit_;
+    LOG(INFO) << pid_name_ << " param read complete!";
+    LOG_IF(WARNING, 1) << "Kp: " << kp_ << "\tKi: " << ki_ << "\tKd: " << kd_
+                       << "\tPid.Name: " << pid_name_
+                       << "\tOutput.Limit: " << output_limit_;
+  };
+  ~PidParams() = default;
+  std::string pid_name_;
+  double kp_{0.0}, ki_{0.0}, kd_{0.0};
+  double last_error_{0.0};
+  double output_limit_{0.0};
 };
 
 class BikePid {
@@ -24,9 +43,9 @@ class BikePid {
   }
 
  private:
-  std::shared_ptr<PidParams> angle_pid_;
-  std::shared_ptr<PidParams> angle_vel_pid_;
-  std::shared_ptr<PidParams> speed_pid_;
+  std::shared_ptr<PidParams> angle_pid_ptr_;
+  std::shared_ptr<PidParams> angle_vel_pid_ptr_;
+  std::shared_ptr<PidParams> speed_pid_ptr_;
 };
 
 #endif  // BIKE_PID_HPP
--- bike_core/src/BikePid.cpp
+++ bike_core/src/BikePid.cpp
@@ -1,8 +1,9 @@
 #include "bike_core/BikePid.hpp"
 
-BikePid::BikePid()
-{
-    LOG(INFO) << "Bike PID";
+BikePid::BikePid() {
+  LOG(INFO) << "Bike PID";
+  const std::string speed_pid_file = "/home/hll/code_space/bike_ws/src/bike_core/params/bike_speed_pid.yaml";
+  speed_pid_ptr_ = std::make_shared<PidParams>(speed_pid_file);
 }
 
 float BikePid::operator()(const float target, float current) const {
