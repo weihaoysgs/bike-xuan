17dd4c844922acf70d3eb29e93011199b3c54378
weihao <weihao_ysgs@163.com>
2022-11-01 13:36:57 +0800

    add sbus topic publisher

--- bike_core/src/BikeXuanControl.cpp
+++ bike_core/src/BikeXuanControl.cpp
@@ -19,6 +19,9 @@ BikeXuanControl::BikeXuanControl() : nh_("~") {
   odrive_axis1_can_parsed_msg_ptr_ =
       std::make_shared<bike_core::odrive_motor_feedback_msg>();
 
+  pub_sbus_channels_value_ =
+      nh_.advertise<bike_core::sbus_channels_msg>("sbus_channel_values", 10);
+
   sub_can_src_msg_ = nh_.subscribe<bike_core::odrive_can_msg>(
       "topic1", 100, &BikeXuanControl::SubOdriveCanSourceMessageCB, this);
   sub_remote_ctrl_msg_ = nh_.subscribe<bike_core::remote_control_msg>(
