787fe27f7b77273a39015bd14ed2b36faf8eac5b
weihao <weihao_ysgs@163.com>
2022-11-01 09:59:12 +0800

    sbus output success

---
+++ bike_core/node/SbusSimulateOutputNode.cpp
@@ -0,0 +1,20 @@
+#include "bike_core/SbusSimulate.hpp"
+#include "qapplication.h"
+
+void InitGlog() {
+  google::InitGoogleLogging("TestServoControl");
+  google::SetLogFilenameExtension("log_");
+  google::SetLogDestination(google::INFO, "log/");
+
+  FLAGS_stderrthreshold = google::INFO;
+  FLAGS_colorlogtostderr = true;
+  FLAGS_max_log_size = 1024;
+  FLAGS_stop_logging_if_full_disk = true;
+}
+int main(int argc, char** argv) {
+  QApplication app(argc, argv);
+  InitGlog();
+  ros::init(argc, argv, "subs_simulate_node");
+  SbusSimulateSerial sbus_simu_ser;
+  return 0;
+}
